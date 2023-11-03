14b3822ede1babeefda3a66fe85195e143536375
Vedant-N421 <147396311+Vedant-N421@users.noreply.github.com>
2023-11-03 10:57:43 +0000

    finished CRUDDING?

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -4,7 +4,7 @@ import org.mongodb.scala._
 import models.DataModel
 import org.mongodb.scala.result.DeleteResult
 import play.api.libs.json.Format.GenericFormat
-import play.api.libs.json.Json
+import play.api.libs.json.{JsError, JsSuccess, JsValue, Json}
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 import repositories.DataRepository
 
@@ -13,9 +13,21 @@ import scala.concurrent.Future
 
 class ApplicationController @Inject()(val controllerComponents: ControllerComponents, val dataRepository: DataRepository) extends BaseController{
 
-  def create(id: String) = TODO
+  def create(): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[DataModel] match {
+      case JsSuccess(dataModel, _) =>
+        dataRepository.create(dataModel).map(_ => Created)
+      case JsError(_) => Future(BadRequest)
+    }
+  }
 
-  def update(id: String) = TODO
+  def update(id: String): Action[JsValue] = Action.async(parse.json){ implicit request =>
+    request.body.validate[DataModel] match{
+      case JsSuccess(dataModel: DataModel, _) =>
+        dataRepository.update(id=id, book=dataModel).map(_ => Accepted)
+      case JsError(_) => Future(BadRequest)
+    }
+  }
 
   //  def index(): Action[AnyContent] = Action {
   //    Ok("It ran!")
@@ -30,22 +42,19 @@ class ApplicationController @Inject()(val controllerComponents: ControllerCompon
     }
   }
 
-  def read(id: String): Action[AnyContent] = dataRepository.read(id: String).map{
-    case item: DataModel => Ok{
-      Json.toJson(item)
+  def read(id: String): Action[AnyContent] = Action.async { implicit request =>
+    dataRepository.read(id: String).map {
+      case item: DataModel => Ok {
+        Json.toJson(item)
+      }
+      case _ => NotFound{Status(_)(Json.toJson("Unable to read book"))}
     }
-    case _ => Status(_)(Json.toJson("Unable to read book"))
   }
 
-  def delete(id: String): Future[Object] =
-    dataRepository.delete(id: String).map{
-      case result: DeleteResult => Accepted
-      case _ => Status(_)(Json.toJson("Unable to delete book"))
+  def delete(id: String): Action[AnyContent] = Action.async { implicit request =>
+    dataRepository.delete(id: String).map {
+      case res: DeleteResult => Accepted
+      case _ => NotFound{Status(_)(Json.toJson("Unable to delete book"))}
     }
-
-
-
-
-
-
+  }
 }

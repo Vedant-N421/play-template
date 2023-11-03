53541b8e37dea89798788ab5af5eab76b52ffd44
Vedant-N421 <147396311+Vedant-N421@users.noreply.github.com>
2023-11-03 16:54:22 +0000

    first commit

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,17 +1,16 @@
 package controllers
 
-import org.mongodb.scala._
 import models.DataModel
 import org.mongodb.scala.result.DeleteResult
-import play.api.libs.json.Format.GenericFormat
+//import play.api.libs.json.Format.GenericFormat
 import play.api.libs.json.{JsError, JsSuccess, JsValue, Json}
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 import repositories.DataRepository
 
 import javax.inject.Inject
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-class ApplicationController @Inject()(val controllerComponents: ControllerComponents, val dataRepository: DataRepository) extends BaseController{
+class ApplicationController @Inject()(val controllerComponents: ControllerComponents, val dataRepository: DataRepository)(implicit val ec: ExecutionContext) extends BaseController{
 
   def create(): Action[JsValue] = Action.async(parse.json) { implicit request =>
     request.body.validate[DataModel] match {
@@ -22,7 +21,7 @@ class ApplicationController @Inject()(val controllerComponents: ControllerCompon
   }
 
   def update(id: String): Action[JsValue] = Action.async(parse.json){ implicit request =>
-    request.body.validate[DataModel] match{
+    request.body.validate[DataModel] match {
       case JsSuccess(dataModel: DataModel, _) =>
         dataRepository.update(id=id, book=dataModel).map(_ => Accepted)
       case JsError(_) => Future(BadRequest)
@@ -35,26 +34,22 @@ class ApplicationController @Inject()(val controllerComponents: ControllerCompon
 
   def index(): Action[AnyContent] = Action.async { implicit request =>
     dataRepository.index().map {
-      case Right(item: Seq[DataModel]) => Ok {
-        Json.toJson(item)
-      }
+      case Right(item: Seq[DataModel]) => Ok(Json.toJson(item))
       case Left(error) => Status(error)(Json.toJson("Unable to find any books"))
     }
   }
 
   def read(id: String): Action[AnyContent] = Action.async { implicit request =>
     dataRepository.read(id: String).map {
-      case item: DataModel => Ok {
-        Json.toJson(item)
-      }
-      case _ => NotFound{Status(_)(Json.toJson("Unable to read book"))}
+      case item: DataModel => Ok(Json.toJson(item))
+      case _ => BadRequest(Json.toJson("Unable to read book"))
     }
   }
 
   def delete(id: String): Action[AnyContent] = Action.async { implicit request =>
     dataRepository.delete(id: String).map {
-      case res: DeleteResult => Accepted
-      case _ => NotFound{Status(_)(Json.toJson("Unable to delete book"))}
+      case _: DeleteResult => Accepted
+      case _ => BadRequest(Json.toJson("Unable to delete book"))
     }
   }
 }
--- app/models/DataModel.scala
+++ app/models/DataModel.scala
@@ -2,13 +2,11 @@ package models
 
 import play.api.libs.json.{Json, OFormat}
 
-class DataModel {
-  case class DataModel(_id: String,
-                       name: String,
-                       description: String,
-                       numSales: Int)
+case class DataModel(_id: String,
+                     name: String,
+                     description: String,
+                     numSales: Int)
 
-  object DataModel {
-    implicit val formats: OFormat[DataModel] = Json.format[DataModel]
-  }
+object DataModel {
+  implicit val formats: OFormat[DataModel] = Json.format[DataModel]
 }
